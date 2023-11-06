f6bd18cfe2548cadae944d8f7cfa217fb893d5ce
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2023-11-06 13:09:28 -0600

    update sizes

--- comms/baseline.cc
+++ comms/baseline.cc
@@ -14,56 +14,82 @@ static char help[] = "Baseline communications for space charge 3D solver! \n\n";
 
 #include <petscvec.h>
 
-int main(int argc,char **argv){
+int main(int argc, char **argv) {
 
-  PetscErrorCode     ierr;           /* Error code */
-  PetscMPIInt        grank;          /* Global MPI rank */
-  PetscMPIInt        gsize;          /* Global MPI size */
-  PetscSubcomm       psubcomm;       /* PETSc sub-communicator */
-  MPI_Comm           subcomm;        /* MPI sub-communicator */
-  PetscInt           nsubcomms = 4;  /* number of sub-communicators */
-  PetscInt           nsize = 12;     /* size of vectors on each rank */
-  Vec                seqrho;         /* seq vector */
-  Vec                seqphi;         /* seq vector */
-  PetscScalar        *h_phi_val;     /* pointer to vector's contents on host */
-  PetscScalar        *h_rho_val;     /* pointer to vector's contents on host */
-  PetscScalar const  *d_phi_val;     /* read-only pointer to vector's contents on device */
-  PetscScalar const  *d_rho_val;     /* read-only pointer to vector's contents on device */
-  PetscMemType       mtype_rho;      /* memory type for the rho vector */
-  PetscMemType       mtype_phi;      /* memory type for the phi vector */
-  PetscLogStage      logstages[2];   /*! stages for logging performance */
+  PetscErrorCode ierr;               /* Error code */
+  PetscMPIInt grank;                 /* Global MPI rank */
+  PetscMPIInt gsize;                 /* Global MPI size */
+  PetscSubcomm psubcomm;             /* PETSc sub-communicator */
+  MPI_Comm subcomm;                  /* MPI sub-communicator */
+  PetscInt nsubcomms = 4;            /* number of sub-communicators */
+  PetscInt nsize = 256 * 256 * 1024; /* size of vectors on each rank */
+  Vec seqrho;                        /* seq vector */
+  Vec seqphi;                        /* seq vector */
+  PetscScalar *h_phi_val;            /* pointer to vector's contents on host */
+  PetscScalar *h_rho_val;            /* pointer to vector's contents on host */
+  PetscScalar const
+      *d_phi_val; /* read-only pointer to vector's contents on device */
+  PetscScalar const
+      *d_rho_val;         /* read-only pointer to vector's contents on device */
+  PetscMemType mtype_rho; /* memory type for the rho vector */
+  PetscMemType mtype_phi; /* memory type for the phi vector */
+  PetscLogStage logstages[2]; /*! stages for logging performance */
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Initialize PETSc and MPI, query mpi rank and size
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
-  ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &grank);CHKERRMPI(ierr);
-  ierr = MPI_Comm_size(PETSC_COMM_WORLD, &gsize);CHKERRMPI(ierr);
+  ierr = PetscInitialize(&argc, &argv, (char *)0, help);
+  if (ierr)
+    return ierr;
+  ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &grank);
+  CHKERRMPI(ierr);
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD, &gsize);
+  CHKERRMPI(ierr);
 
-  ierr = PetscLogStageRegister(const_cast<char*>("world-allreduce"), &logstages[0]);CHKERRQ(ierr);
-  ierr = PetscLogStageRegister(const_cast<char*>("scomm-allreduce"), &logstages[1]);CHKERRQ(ierr);
+  ierr = PetscLogStageRegister(const_cast<char *>("world-allreduce"),
+                               &logstages[0]);
+  CHKERRQ(ierr);
+  ierr = PetscLogStageRegister(const_cast<char *>("scomm-allreduce"),
+                               &logstages[1]);
+  CHKERRQ(ierr);
 
-  ierr = PetscSubcommCreate(PETSC_COMM_WORLD, &(psubcomm));CHKERRQ(ierr);
-  ierr = PetscSubcommSetNumber(psubcomm, nsubcomms);CHKERRQ(ierr);
-  ierr = PetscSubcommSetType(psubcomm, PETSC_SUBCOMM_CONTIGUOUS);CHKERRQ(ierr);
-  ierr = PetscSubcommGetChild(psubcomm, &(subcomm));CHKERRQ(ierr);
+  ierr = PetscSubcommCreate(PETSC_COMM_WORLD, &(psubcomm));
+  CHKERRQ(ierr);
+  ierr = PetscSubcommSetNumber(psubcomm, nsubcomms);
+  CHKERRQ(ierr);
+  ierr = PetscSubcommSetType(psubcomm, PETSC_SUBCOMM_CONTIGUOUS);
+  CHKERRQ(ierr);
+  ierr = PetscSubcommGetChild(psubcomm, &(subcomm));
+  CHKERRQ(ierr);
 
-  ierr = VecCreate(PETSC_COMM_SELF, &seqrho);CHKERRQ(ierr);
-  ierr = VecCreate(PETSC_COMM_SELF, &seqphi);CHKERRQ(ierr);
-  ierr = VecSetSizes(seqrho, PETSC_DECIDE, nsize);CHKERRQ(ierr);
-  ierr = VecSetSizes(seqphi, PETSC_DECIDE, nsize);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(seqrho);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(seqphi);CHKERRQ(ierr);
+  ierr = VecCreate(PETSC_COMM_SELF, &seqrho);
+  CHKERRQ(ierr);
+  ierr = VecCreate(PETSC_COMM_SELF, &seqphi);
+  CHKERRQ(ierr);
+  ierr = VecSetSizes(seqrho, PETSC_DECIDE, nsize);
+  CHKERRQ(ierr);
+  ierr = VecSetSizes(seqphi, PETSC_DECIDE, nsize);
+  CHKERRQ(ierr);
+  ierr = VecSetFromOptions(seqrho);
+  CHKERRQ(ierr);
+  ierr = VecSetFromOptions(seqphi);
+  CHKERRQ(ierr);
 
   /* Initialize rho and phi on each rank to random values */
-  ierr = VecSetRandom(seqrho, NULL);CHKERRQ(ierr);
-  ierr = VecSetRandom(seqphi, NULL);CHKERRQ(ierr);
+  ierr = VecSetRandom(seqrho, NULL);
+  CHKERRQ(ierr);
+  ierr = VecSetRandom(seqphi, NULL);
+  CHKERRQ(ierr);
 
   /* Get access to local vector on device, makes vector reside on device */
-  ierr = VecGetArrayReadAndMemType(seqrho, &d_rho_val, &mtype_rho);CHKERRQ(ierr);
-  ierr = VecGetArrayReadAndMemType(seqphi, &d_phi_val, &mtype_phi);CHKERRQ(ierr);
-  ierr = VecRestoreArrayReadAndMemType(seqrho, &d_rho_val);CHKERRQ(ierr);
-  ierr = VecRestoreArrayReadAndMemType(seqphi, &d_phi_val);CHKERRQ(ierr);
+  ierr = VecGetArrayReadAndMemType(seqrho, &d_rho_val, &mtype_rho);
+  CHKERRQ(ierr);
+  ierr = VecGetArrayReadAndMemType(seqphi, &d_phi_val, &mtype_phi);
+  CHKERRQ(ierr);
+  ierr = VecRestoreArrayReadAndMemType(seqrho, &d_rho_val);
+  CHKERRQ(ierr);
+  ierr = VecRestoreArrayReadAndMemType(seqphi, &d_phi_val);
+  CHKERRQ(ierr);
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      First all-reduce over global communicator!
@@ -72,21 +98,23 @@ int main(int argc,char **argv){
   /* No warm phase is needed as we do not use persistent collectives
    * which have been introduced in MPI-4! */
 
-  ierr= PetscLogStagePush(logstages[0]);CHKERRQ(ierr);
+  ierr = PetscLogStagePush(logstages[0]);
+  CHKERRQ(ierr);
 
   /* Get access to local vector on host, makes vector reside on host */
-  ierr = VecGetArray(seqrho, &h_rho_val);CHKERRQ(ierr);
-  ierr = MPIU_Allreduce(MPI_IN_PLACE,
-      h_rho_val,
-      nsize,
-      MPIU_SCALAR,
-      MPI_SUM,
-      PETSC_COMM_WORLD);CHKERRMPI(ierr);
-  ierr = VecRestoreArray(seqrho, &h_rho_val);CHKERRQ(ierr);
+  ierr = VecGetArray(seqrho, &h_rho_val);
+  CHKERRQ(ierr);
+  ierr = MPIU_Allreduce(MPI_IN_PLACE, h_rho_val, nsize, MPIU_SCALAR, MPI_SUM,
+                        PETSC_COMM_WORLD);
+  CHKERRMPI(ierr);
+  ierr = VecRestoreArray(seqrho, &h_rho_val);
+  CHKERRQ(ierr);
 
   /* Get access to local vector on device, makes vector reside on device */
-  ierr = VecGetArrayReadAndMemType(seqrho, &d_rho_val, &mtype_rho);CHKERRQ(ierr);
-  ierr = VecRestoreArrayReadAndMemType(seqrho, &d_rho_val);CHKERRQ(ierr);
+  ierr = VecGetArrayReadAndMemType(seqrho, &d_rho_val, &mtype_rho);
+  CHKERRQ(ierr);
+  ierr = VecRestoreArrayReadAndMemType(seqrho, &d_rho_val);
+  CHKERRQ(ierr);
 
   ierr = PetscLogStagePop();
 
@@ -94,28 +122,33 @@ int main(int argc,char **argv){
      Second all-reduce over sub-communicator!
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-  ierr= PetscLogStagePush(logstages[1]);CHKERRQ(ierr);
+  ierr = PetscLogStagePush(logstages[1]);
+  CHKERRQ(ierr);
 
   /* Get access to local vector on host, makes vector reside on host */
-  ierr = VecGetArray(seqphi, &h_phi_val);CHKERRQ(ierr);
-  ierr = MPIU_Allreduce(MPI_IN_PLACE,
-      h_phi_val,
-      nsize,
-      MPIU_SCALAR,
-      MPI_SUM,
-      subcomm);CHKERRMPI(ierr);
-  ierr = VecRestoreArray(seqphi, &h_phi_val);CHKERRQ(ierr);
+  ierr = VecGetArray(seqphi, &h_phi_val);
+  CHKERRQ(ierr);
+  ierr = MPIU_Allreduce(MPI_IN_PLACE, h_phi_val, nsize, MPIU_SCALAR, MPI_SUM,
+                        subcomm);
+  CHKERRMPI(ierr);
+  ierr = VecRestoreArray(seqphi, &h_phi_val);
+  CHKERRQ(ierr);
 
   /* Get access to local vector on device, makes vector reside on device */
-  ierr = VecGetArrayReadAndMemType(seqphi, &d_phi_val, &mtype_phi);CHKERRQ(ierr);
-  ierr = VecRestoreArrayReadAndMemType(seqphi, &d_phi_val);CHKERRQ(ierr);
+  ierr = VecGetArrayReadAndMemType(seqphi, &d_phi_val, &mtype_phi);
+  CHKERRQ(ierr);
+  ierr = VecRestoreArrayReadAndMemType(seqphi, &d_phi_val);
+  CHKERRQ(ierr);
 
   ierr = PetscLogStagePop();
 
   /* cleanup */
-  ierr = VecDestroy(&seqrho);CHKERRQ(ierr);
-  ierr = VecDestroy(&seqphi);CHKERRQ(ierr);
-  ierr = PetscSubcommDestroy(&psubcomm);CHKERRQ(ierr);
+  ierr = VecDestroy(&seqrho);
+  CHKERRQ(ierr);
+  ierr = VecDestroy(&seqphi);
+  CHKERRQ(ierr);
+  ierr = PetscSubcommDestroy(&psubcomm);
+  CHKERRQ(ierr);
 
   ierr = PetscFinalize();
   return ierr;
--- solve/3D/poisson3d.c
+++ solve/3D/poisson3d.c
@@ -12,98 +12,135 @@ static char help[] = "Solves 3D Poisson \n\n";
 
 #include "poisson3d.h"
 
-int main(int argc,char **argv)
-{
-  AppCtx         appctx;     /* user-defined application context */
-  KSP            ksp;        /* krylov solver */
-  PetscInt       it;         /* index */
-  PetscViewer    rhoviewer;  /* hdf5 file viewer for rho */
-  PetscViewer    phiviewer;  /* hdf5 file viewer for phi */
-  PetscLogStage  solvestage; /* log stage for logging linear solve */
-  PetscErrorCode ierr;       /* Error code */
+int main(int argc, char **argv) {
+  AppCtx appctx;            /* user-defined application context */
+  KSP ksp;                  /* krylov solver */
+  PetscInt it;              /* index */
+  PetscViewer rhoviewer;    /* hdf5 file viewer for rho */
+  PetscViewer phiviewer;    /* hdf5 file viewer for phi */
+  PetscLogStage solvestage; /* log stage for logging linear solve */
+  PetscErrorCode ierr;      /* Error code */
 
-  ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
-  ierr = PetscLogStageRegister("linear-solve",&solvestage);CHKERRQ(ierr);
+  ierr = PetscInitialize(&argc, &argv, (char *)0, help);
+  if (ierr)
+    return ierr;
+  ierr = PetscLogStageRegister("linear-solve", &solvestage);
+  CHKERRQ(ierr);
 
   appctx.Lx = 1e-3;
   appctx.Ly = 1e-3;
   appctx.Lz = 1e-3;
 
-  ierr = DMDACreate3d(PETSC_COMM_WORLD,
-      DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
-      DMDA_STENCIL_STAR, 64, 64, 256,
-      PETSC_DECIDE, PETSC_DECIDE, PETSC_DECIDE, 1, 1,
-      NULL,NULL,NULL, &appctx.da);CHKERRQ(ierr);
-  ierr = DMSetFromOptions(appctx.da);CHKERRQ(ierr);
-  ierr = DMSetUp(appctx.da);CHKERRQ(ierr);
-  ierr = DMDASetUniformCoordinates(appctx.da, -appctx.Lx, appctx.Lx, -appctx.Ly, appctx.Ly, -appctx.Lz, appctx.Lz);CHKERRQ(ierr);
+  ierr = DMDACreate3d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
+                      DM_BOUNDARY_NONE, DMDA_STENCIL_STAR, 256, 256, 1024,
+                      PETSC_DECIDE, PETSC_DECIDE, PETSC_DECIDE, 1, 1, NULL,
+                      NULL, NULL, &appctx.da);
+  CHKERRQ(ierr);
+  ierr = DMSetFromOptions(appctx.da);
+  CHKERRQ(ierr);
+  ierr = DMSetUp(appctx.da);
+  CHKERRQ(ierr);
+  ierr = DMDASetUniformCoordinates(appctx.da, -appctx.Lx, appctx.Lx, -appctx.Ly,
+                                   appctx.Ly, -appctx.Lz, appctx.Lz);
+  CHKERRQ(ierr);
 
   /* create discretization matrix */
-  ierr = DMCreateMatrix(appctx.da, &(appctx.A));CHKERRQ(ierr);
+  ierr = DMCreateMatrix(appctx.da, &(appctx.A));
+  CHKERRQ(ierr);
 
   /* create rho and phi vectors */
-  ierr = DMCreateGlobalVector(appctx.da, &appctx.rho);CHKERRQ(ierr);
-  ierr = PetscObjectSetName((PetscObject)appctx.rho, "rho_vec");CHKERRQ(ierr);
-  ierr = DMCreateGlobalVector(appctx.da, &appctx.phi);CHKERRQ(ierr);
-  ierr = PetscObjectSetName((PetscObject)appctx.phi, "phi_vec");CHKERRQ(ierr);
-  ierr = VecSet(appctx.phi, 0.0);CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(appctx.da, &appctx.rho);
+  CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject)appctx.rho, "rho_vec");
+  CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(appctx.da, &appctx.phi);
+  CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject)appctx.phi, "phi_vec");
+  CHKERRQ(ierr);
+  ierr = VecSet(appctx.phi, 0.0);
+  CHKERRQ(ierr);
 
   /* fill matrix */
-  ierr = ComputeMatrix(&appctx);CHKERRQ(ierr);
+  ierr = ComputeMatrix(&appctx);
+  CHKERRQ(ierr);
 
   /* fill rho vector */
-  ierr = ComputeRHSGaussian(&appctx);CHKERRQ(ierr);
-  //ierr = ComputeRHSPoint(&appctx);CHKERRQ(ierr);
+  ierr = ComputeRHSGaussian(&appctx);
+  CHKERRQ(ierr);
+  // ierr = ComputeRHSPoint(&appctx);CHKERRQ(ierr);
 
   /* SI units for Poisson eq. */
   appctx.eps0 = 8.85418781281e-12;
-  ierr = VecScale(appctx.rho, (1/appctx.eps0));CHKERRQ(ierr);
+  ierr = VecScale(appctx.rho, (1 / appctx.eps0));
+  CHKERRQ(ierr);
 
   /* Scaling factor of hx*hy*hz */
-  ierr = VecScale(appctx.rho, 1/(appctx.Lx*appctx.Ly*appctx.Lz));CHKERRQ(ierr);
+  ierr = VecScale(appctx.rho, 1 / (appctx.Lx * appctx.Ly * appctx.Lz));
+  CHKERRQ(ierr);
 
   /* create krylov solver */
-  ierr = KSPCreate(PETSC_COMM_WORLD, &ksp);CHKERRQ(ierr);
-  ierr = KSPSetOperators(ksp, appctx.A, appctx.A);CHKERRQ(ierr);
-  ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
-  ierr = KSPSetUp(ksp);CHKERRQ(ierr);
-  ierr = KSPSetDM(ksp, appctx.da);CHKERRQ(ierr);
+  ierr = KSPCreate(PETSC_COMM_WORLD, &ksp);
+  CHKERRQ(ierr);
+  ierr = KSPSetOperators(ksp, appctx.A, appctx.A);
+  CHKERRQ(ierr);
+  ierr = KSPSetFromOptions(ksp);
+  CHKERRQ(ierr);
+  ierr = KSPSetUp(ksp);
+  CHKERRQ(ierr);
+  ierr = KSPSetDM(ksp, appctx.da);
+  CHKERRQ(ierr);
 
   /* solve - warm up */
-  ierr = KSPSolve(ksp, appctx.rho, appctx.phi);CHKERRQ(ierr);
+  ierr = KSPSolve(ksp, appctx.rho, appctx.phi);
+  CHKERRQ(ierr);
   /* reuse preconditioner in subsequent solves */
-  ierr = KSPSetReusePreconditioner(ksp, PETSC_TRUE);CHKERRQ(ierr);
+  ierr = KSPSetReusePreconditioner(ksp, PETSC_TRUE);
+  CHKERRQ(ierr);
 
-  PetscLogStagePush(solvestage);CHKERRQ(ierr);
-  for (it=0; it<5; it++){
-    ierr = KSPSolve(ksp, appctx.rho, appctx.phi);CHKERRQ(ierr);
+  PetscLogStagePush(solvestage);
+  CHKERRQ(ierr);
+  for (it = 0; it < 5; it++) {
+    ierr = KSPSolve(ksp, appctx.rho, appctx.phi);
+    CHKERRQ(ierr);
   }
-  PetscLogStagePop();CHKERRQ(ierr);
+  PetscLogStagePop();
+  CHKERRQ(ierr);
 
   /* Scaling factor of hx*hy*hz */
-  ierr = VecScale(appctx.rho, (appctx.Lx*appctx.Ly*appctx.Lz));CHKERRQ(ierr);
-  ierr = VecScale(appctx.phi, (appctx.Lx*appctx.Ly*appctx.Lz));CHKERRQ(ierr);
+  ierr = VecScale(appctx.rho, (appctx.Lx * appctx.Ly * appctx.Lz));
+  CHKERRQ(ierr);
+  ierr = VecScale(appctx.phi, (appctx.Lx * appctx.Ly * appctx.Lz));
+  CHKERRQ(ierr);
 
   /* write rho and phi to hdf5 files */
-  ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"rho.h5",
-      FILE_MODE_WRITE,&rhoviewer);CHKERRQ(ierr);
-  ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"phi.h5",
-      FILE_MODE_WRITE,&phiviewer);CHKERRQ(ierr);
+  ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD, "rho.h5", FILE_MODE_WRITE,
+                             &rhoviewer);
+  CHKERRQ(ierr);
+  ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD, "phi.h5", FILE_MODE_WRITE,
+                             &phiviewer);
+  CHKERRQ(ierr);
 
-  ierr = VecView(appctx.rho, rhoviewer);CHKERRQ(ierr);
-  ierr = VecView(appctx.phi, phiviewer);CHKERRQ(ierr);
+  ierr = VecView(appctx.rho, rhoviewer);
+  CHKERRQ(ierr);
+  ierr = VecView(appctx.phi, phiviewer);
+  CHKERRQ(ierr);
 
-  ierr = PetscViewerDestroy(&rhoviewer);CHKERRQ(ierr);
-  ierr = PetscViewerDestroy(&phiviewer);CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(&rhoviewer);
+  CHKERRQ(ierr);
+  ierr = PetscViewerDestroy(&phiviewer);
+  CHKERRQ(ierr);
 
   /* cleanup */
-  ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
-  ierr = VecDestroy(&appctx.phi);CHKERRQ(ierr);
-  ierr = VecDestroy(&appctx.rho);CHKERRQ(ierr);
-  ierr = MatDestroy(&appctx.A);CHKERRQ(ierr);
-  ierr = DMDestroy(&appctx.da);CHKERRQ(ierr);
+  ierr = KSPDestroy(&ksp);
+  CHKERRQ(ierr);
+  ierr = VecDestroy(&appctx.phi);
+  CHKERRQ(ierr);
+  ierr = VecDestroy(&appctx.rho);
+  CHKERRQ(ierr);
+  ierr = MatDestroy(&appctx.A);
+  CHKERRQ(ierr);
+  ierr = DMDestroy(&appctx.da);
+  CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
 }
-
-
